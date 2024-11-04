53e5362af25829bc17bf381ef4ce33b15ea95342
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-11-04 20:11:12 +0700

    global widgets: add star rate widget

---
+++ restaurant_review/lib/global_widgets/ratings/star_rate.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_review/constants/colors.dart';
+
+class StarRating extends StatelessWidget {
+  final double value;
+  final bool isReadOnly;
+  final void Function(int) onPressed;
+  final double size;
+
+  const StarRating({
+    super.key,
+    required this.value,
+    this.isReadOnly = true,
+    required this.onPressed,
+    this.size = 24,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(5, (index) {
+        return GestureDetector(
+          onTap: isReadOnly ? null : () => onPressed(index),
+          child: Icon(
+            Icons.star,
+            color: index <= value ? AppColors.rateYellow : AppColors.rateGray,
+            size: size,
+          ),
+        );
+      }),
+    );
+  }
+}
