9832657545fbc7ae708c75ed6ba1905baf824dcf
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-11-05 09:58:24 +0700

    global widgets: add rich text editor

---
+++ restaurant_review/lib/global_widgets/input_fields/rich_text_display.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:restaurant_review/constants/colors.dart';
+import 'package:restaurant_review/global_widgets/input_fields/rich_text_editor.dart';
+
+class RichTextDisplayController extends GetxController {
+  var quillController = quill.QuillController.basic().obs;
+
+  void setContent(List<dynamic> content) {
+    quillController.value.document = quill.Document.fromJson(content);
+  }
+}
+
+class RichTextDisplay extends StatelessWidget {
+  final List<dynamic> content;
+  final ValueChanged<String?> onContentChanged;
+
+  const RichTextDisplay({
+    super.key,
+    required this.content,
+    required this.onContentChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final RichTextDisplayController controller =
+        Get.put(RichTextDisplayController());
+
+    controller.setContent(content);
+
+    return GestureDetector(
+      onTap: () async {
+        final result = await Get.to(() => MyRichTextEditor(
+              initialContent: content,
+            ));
+
+        if (result != null) {
+          onContentChanged(result);
+          controller.setContent(jsonDecode(result));
+        }
+      },
+      child: AbsorbPointer(
+        // This makes sure the editor does not gain focus or handle taps
+        child: Container(
+          padding: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: AppColors.textBlack, width: 0.5),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Obx(() => quill.QuillEditor.basic(
+              controller: controller.quillController.value)),
+        ),
+      ),
+    );
+  }
+}
---
+++ restaurant_review/lib/global_widgets/input_fields/rich_text_editor.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:get/get.dart';
+import 'package:restaurant_review/constants/font_sizes.dart';
+import 'package:restaurant_review/global_widgets/modals/modals.dart';
+
+class MyRichTextEditorController extends GetxController {
+  final quillController = quill.QuillController.basic();
+  final focusNode = FocusNode();
+
+  String getQuillContentAsJson() {
+    return jsonEncode(quillController.document.toDelta().toJson());
+  }
+
+  @override
+  void onClose() {
+    quillController.dispose();
+    focusNode.dispose();
+    super.onClose();
+  }
+}
+
+class MyRichTextEditor extends StatelessWidget {
+  final List<dynamic>? initialContent;
+
+  const MyRichTextEditor({super.key, this.initialContent});
+
+  @override
+  Widget build(BuildContext context) {
+    final MyRichTextEditorController controller =
+        Get.put(MyRichTextEditorController());
+
+    if (initialContent != null) {
+      controller.quillController.document =
+          quill.Document.fromJson(initialContent!);
+    }
+
+    // Request focus on the text editor when the widget is built
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      controller.focusNode.requestFocus();
+    });
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            ModalUtils.showMessageWithButtonsModal(
+              FlutterI18n.translate(context, "rich_text_editor.discard_title"),
+              FlutterI18n.translate(
+                  context, "rich_text_editor.discard_content"),
+              () {
+                Get.back();
+                Get.back(result: null);
+              },
+            );
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              String content = controller.getQuillContentAsJson();
+              Get.back(result: content);
+            },
+            child: Text(
+              FlutterI18n.translate(context, "rich_text_editor.save"),
+              style: const TextStyle(
+                fontSize: AppFontSizes.s7,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(width: 5),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: quill.QuillEditor(
+                controller: controller.quillController,
+                scrollController: ScrollController(),
+                focusNode: controller.focusNode,
+              ),
+            ),
+            quill.QuillToolbar.simple(controller: controller.quillController),
+          ],
+        ),
+      ),
+    );
+  }
+}
