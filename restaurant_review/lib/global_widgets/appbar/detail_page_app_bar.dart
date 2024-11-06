92df0b126ea5c80e09ba28e16dbf1ecd37db2093
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-11-06 19:12:51 +0700

    global widgets: add detail page appbar

---
+++ restaurant_review/lib/global_widgets/appbar/detail_page_app_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_review/constants/font_sizes.dart';
+
+class DetailPageAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final String buttonLabel;
+  final VoidCallback onPressed;
+  const DetailPageAppBar({
+    super.key,
+    required this.title,
+    required this.buttonLabel,
+    required this.onPressed,
+  });
+  @override
+  AppBar build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      centerTitle: true,
+      actions: [
+        TextButton(
+          onPressed: onPressed,
+          child: Text(
+            buttonLabel,
+            style: const TextStyle(
+                fontSize: AppFontSizes.s7, fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(width: 5),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
