46d945eaabc45644bf6f47c27422f1bce8ef275b
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-11-12 20:54:17 +0700

    splash module: add user id to shared preferences

--- restaurant_review/lib/modules/splash/controller/splash_controller.dart
+++ restaurant_review/lib/modules/splash/controller/splash_controller.dart
@@ -1,7 +1,8 @@
 // splash_controller.dart
 import 'package:get/get.dart';
 import 'package:restaurant_review/routes/routes.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:restaurant_review/services/supabase.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashController extends GetxController {
   @override
@@ -12,11 +13,14 @@ class SplashController extends GetxController {
 
   Future<void> _redirect() async {
     await Future.delayed(Duration.zero);
-    final session = Supabase.instance.client.auth.currentSession;
+    final session = supabase.auth.currentSession;
 
+    final prefs = await SharedPreferences.getInstance();
     if (session != null) {
+      await prefs.setString('sessionId', session.user.id);
       Get.offNamed(Routes.home);
     } else {
+      await prefs.remove('sessionId');
       Get.offNamed(Routes.signIn);
     }
   }
