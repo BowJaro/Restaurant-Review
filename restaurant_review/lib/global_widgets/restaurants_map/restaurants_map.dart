f22fcaedeb1e02a3461e639eb01ce3cbceb6dd32
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-12-24 16:19:02 +0700

    restaurant map: fix multiple instances

--- restaurant_review/lib/global_widgets/restaurants_map/restaurants_map.dart
+++ restaurant_review/lib/global_widgets/restaurants_map/restaurants_map.dart
@@ -305,7 +305,7 @@ import 'package:restaurant_review/global_classes/map_restaurant.dart';
 class RestaurantsMapController extends GetxController {
   var restaurants = <MapRestaurantModel>[].obs;
   final mapCenter =
-      LatLng(10.8231, 106.6297).obs; // Default to a specific location
+      const LatLng(10.8231, 106.6297).obs; // Default to a specific location
 
   @override
   void onInit() async {
@@ -368,10 +368,11 @@ class RestaurantsMapController extends GetxController {
 }
 
 class RestaurantsMap extends StatelessWidget {
-  final RestaurantsMapController controller =
-      Get.put(RestaurantsMapController());
+  late final RestaurantsMapController controller;
 
   RestaurantsMap({super.key, required List<MapRestaurantModel> restaurants}) {
+    Get.create(() => RestaurantsMapController());
+    controller = Get.find<RestaurantsMapController>();
     controller.loadRestaurants(restaurants);
   }
 
